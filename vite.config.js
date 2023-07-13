cf085bdaa44b142a5430227bd97aa259edd5ad68
Andres Felipe Zea Acevedo <andres.zea@evertecinc.com>
2023-07-13 18:14:06 -0500

    Vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
     server: { https: true },
     plugins: [
+        basicSsl(),
         vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
